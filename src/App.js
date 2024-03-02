2c0ec21635c0b88eeb5c257b5f3dafc4f2ee117b
muhammad sani <muhammadsanijae@gmail.com>
2024-03-02 15:44:16 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// Import relevarnt Packages and Data file
+import React, {useState} from 'react'
+import './App.css'
+import Data from './Data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  // Create Hooks to control Data items
+  const [search, setSearch] = useState('')
+  const [filterCard,setFilterCard] = useState(Data)
+
+// Created seacrh control and action button
+ const filterData =(e)=>{
+    e.preventDefault()
+    // verify weither the search box is empty
+    if(search === "") return filterCard(Data)
+
+    // Search the desire item from list
+    const filterBySearch = Data.filter((item)=>{
+      return item.title.toLowerCase().includes(search.toLowerCase())
+    })
+    // Update the body with new item
+    setFilterCard(filterBySearch)
 }
 
-export default App;
+  return (
+    <div className='app'>
+     <div className='nav'>
+      <h3>Gospelkick Store</h3>
+      <form className='form' onSubmit={filterData}>
+        <input type='text' className='input' onChange={(e)=>setSearch(e.target.value)} placeholder='Search' />
+        <button type='submit'>Send</button>
+      </form>
+     </div>
+     <div className='body'>
+      {filterCard.map((data,i)=>{
+        return(
+          <div className='card' key={i}>
+            <p className='card_title'>{data.title}</p>
+            <p className='card_subtitle'>{data.subtitle}</p>
+          </div>
+        )
+      })}
+     </div>
+     <div className='footer'>
+      <p>Copyrighted &nbsp; &copy; &nbsp; {new Date().getFullYear()} &nbsp; Gospelkick Store All rigth reserved</p>
+     </div>
+    </div>
+  )
+}
---
+++ src/Data.js
@@ -0,0 +1,16 @@
+const Data = [
+    {
+        title: 'Meet',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien.',
+    }
+    ,
+    {
+        title: 'Rice',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien.',
+    },
+    {
+        title: 'Drinks',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien.',
+    },
+]
+module.exports = Data
